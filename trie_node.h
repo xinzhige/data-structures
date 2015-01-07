63a164e10e140946e67a7da23e093c576480c113
Isaiah Xin <xinzhige8@gmail.com>
2015-01-07 09:04:18 -0800

    Create trie_node.h

---
+++ trie_node.h
@@ -0,0 +1,18 @@
+template <int Size>
+class trieNode {
+public:
+  trieNode(): terminableSize(0), nodeSize(0) {
+    for (int i = 0; i < Size; ++i) {
+      children[i] = nullptr;
+    }
+  }
+  ~trieNode() {
+    for (int i = 0; i < Size; ++i) {
+      delete children [i];
+      children[i] = nullptr; 
+    }
+  }
+  int terminableSize; //store the number of words that end at this node
+  int nodeSize;  // store the number of children of this node
+  trieNode * children[Size];
+};
