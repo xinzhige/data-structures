43f69d3689fb63702b6d73043f7a1e07e65fd7be
Isaiah Zhige Xin <xinzhige8@gmail.com>
2016-01-13 21:51:11 -0800

    Update disj_sets.h

--- disj_sets.h
+++ disj_sets.h
@@ -1,10 +1,6 @@
 #ifndef DISJ_SETS_H_
 #define  DISJ_SETS_H_
 
-
-
-#include <vector>
-
 class DisjSets {
 public:
   explicit DisjSets(int numElements) : s(numElements, -1) { };
@@ -15,32 +11,5 @@ public:
 private:
   std::vector<int> s;
 };
-
-int DisjSets::find(int x) {
-  if (s[x] < 0) {
-    return x;
-  } else {
-    return s[x] = find(s[x]);
-  }
-}
-
-int DisjSets::find(int x) const {
-  if (s[x] <0) {
-    return x;
-  } else {
-    return find(s[x]);
-  }
-}
-
-void DisjSets::unionSets(int root1, int root2) {
-  if (s[root2] < s[root1]) {
-    s[root1] = root2;
-  } else {
-    if (s[root1] == s[root2]) {
-      s[root1] -= 1;
-    }
-    s[root2] = root1;
-  } 
-}
 
 #endif
